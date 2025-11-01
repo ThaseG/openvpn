4b71baf5c7ef0d98962e07fbf625b4cc904c0656
Andrej Hyben <andrej.hyben@ext.ec.europa.eu>
2025-11-01 19:25:14 +0100

    Updated with exporter update

--- Exporter/main.go
+++ Exporter/main.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/go-kit/kit/log"
+	"github.com/go-kit/log"
 	"github.com/go-kit/log/level"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
